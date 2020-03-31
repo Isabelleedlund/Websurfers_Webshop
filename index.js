281e4ae474bf04217fc17751c4d013250b778baf
[isabelleedlund] <[edlund.isabelle@gmail.com]>
2020-03-31 15:10:41 +0200

    admin info

--- index.js
+++ index.js
@@ -6,12 +6,28 @@ const Admin           = require("./router/adminRouter");
 const path            = require("path");
 const app             = express();
 const cookieparser    = require("cookie-parser");
+const stripe          = require("stripe")("sk_test_faWq8pM0PWlpcbv8XMMXYC8C00lfXcRpPM");
+
+
+
+
+// Stripe Payment \\
+const paymentIntent = await stripe.paymentIntents.create({
+  amount: Number,
+  currency: "sek",
+  metadata: {integration_check: "accept_a_payment"}
+})
+
+const stripe = Stripe('pk_test_YdqKsq7KM50ZZyi98Os90Y3A000xvgdY8x');
+const elements = stripe.elements();
+
 
 app.use(cookieparser());
 
 app.use(express.urlencoded({
   extended: true
 }));
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
