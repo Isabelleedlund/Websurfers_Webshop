60d97a933104555203b082f667492c66d182cd7d
[isabelleedlund] <[edlund.isabelle@gmail.com]>
2020-04-03 12:03:35 +0200

    vet inte vad som händer?

--- config/config.js
+++ config/config.js
@@ -3,8 +3,6 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const config = {
     databaseURL: process.env.DATABASE,
     mail: process.env.MAIL,
-    // publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    // clientSecret: paymentIntent.client_secret
 };
 
 module.exports = config;
